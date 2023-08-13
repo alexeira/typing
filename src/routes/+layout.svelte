053410d7bca0adbaae978c77142e06638da8ba5b
alexeira <alex.pincheira06@gmail.com>
2023-08-12 23:44:54 -0400

    setup layout route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,30 @@
 	<title>typing</title>
 </svelte:head>
 
-<div>
+<div class="layout">
+	<nav>
+		<h1>⌨️ typing</h1>
+	</nav>
+
 	<main>
 		<slot />
 	</main>
 </div>
+
+<style>
+	h1 {
+		font-family: 'Lexend Deca', sans-serif;
+		font-size: 1.8rem;
+		letter-spacing: 2px;
+	}
+
+	.layout {
+		width: min(90%, 100ch);
+		margin-inline: auto;
+		height: 100%;
+		display: grid;
+		grid-template-rows: auto 1fr;
+		align-items: center;
+		padding: 2rem;
+	}
+</style>
